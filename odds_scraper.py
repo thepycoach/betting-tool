a00cee60deb1c91a21144727defe7059c6422339
Frank Andrade <55821184+ifrankandrade@users.noreply.github.com>
2021-03-21 21:08:33 -0500

    Update odds_scraper.py

--- odds_scraper.py
+++ odds_scraper.py
@@ -39,98 +39,98 @@ dict_countries = {
 #loop through the dictionary (we're going to open a chrome window for every element of the dictionary)
 for country in dict_countries:
     for league in range(0, len(dict_countries[country])):
-        # execute chromedriver with edited options
-        driver = webdriver.Chrome(path, options=options)
-        driver.get(web)
-        # driver.maximize_window() #when Headless = False
-        # option1
-        # accept = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))
-        # option 2
-        time.sleep(2)
-        accept = driver.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]')
-        accept.click()
+        try:
+            # execute chromedriver with edited options
+            driver = webdriver.Chrome(path, options=options)
+            driver.get(web)
+            driver.maximize_window() #when Headless = False
+            # option1
+            # accept = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]')))
+            # option 2
+            time.sleep(2)
+            accept = driver.find_element_by_xpath('//*[@id="onetrust-accept-btn-handler"]')
+            accept.click()
 
-        #set website language to English
-        language_box = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'ssc-hlsw')))
-        #select dropdown and then value (EN) from dropdown
-        WebDriverWait(language_box, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ssc-hls'))).click()
-        WebDriverWait(language_box, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ssc-en_GB'))).click()
-        #we need to make selenium wait for the website to load after switching languages ---How?---> wait for some element to be loaded in English e.g. "Over/Under 2.5 Goals" text
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//span[contains(text(), "Over/Under 2.5 Goals")]')))
+            #set website language to English
+            language_box = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.CLASS_NAME, 'ssc-hlsw')))
+            #select dropdown and then value (EN) from dropdown
+            WebDriverWait(language_box, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ssc-hls'))).click()
+            WebDriverWait(language_box, 5).until(EC.element_to_be_clickable((By.CLASS_NAME, 'ssc-en_GB'))).click()
+            #we need to make selenium wait for the website to load after switching languages ---How?---> wait for some element to be loaded in English e.g. "Over/Under 2.5 Goals" text
+            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//span[contains(text(), "Over/Under 2.5 Goals")]')))
 
-        #Scraping different competitions across the leagues within the dictionary
-        header = driver.find_element_by_class_name('updated-competitions')
-        competition = WebDriverWait(header, 5).until(EC.element_to_be_clickable((By.XPATH, './/a[contains(@title, "COMPETITIONS")]')))
-        competition.click()
-        competitions_table = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'mod-multipickazmenu-1061-container')))
-        country_button = WebDriverWait(competitions_table, 5).until(EC.element_to_be_clickable((By.XPATH, './/div[contains(@data-category,' +'"' + country + '"' + ')]')))
-        country_button.click()
-        league_button = WebDriverWait(competitions_table, 5).until(EC.element_to_be_clickable((By.XPATH, './/a[contains(@data-galabel,' +'"' + dict_countries[country][league] + '"' + ')]')))
-        league_button.click()
+            #Scraping different competitions across the leagues within the dictionary
+            header = driver.find_element_by_class_name('updated-competitions')
+            competition = WebDriverWait(header, 5).until(EC.element_to_be_clickable((By.XPATH, './/a[contains(@title, "COMPETITIONS")]')))
+            competition.click()
+            competitions_table = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, 'mod-multipickazmenu-1061-container')))
+            country_button = WebDriverWait(competitions_table, 5).until(EC.element_to_be_clickable((By.XPATH, './/div[contains(@data-category,' +'"' + country + '"' + ')]')))
+            country_button.click()
+            league_button = WebDriverWait(competitions_table, 5).until(EC.element_to_be_clickable((By.XPATH, './/a[contains(@data-galabel,' +'"' + dict_countries[country][league] + '"' + ')]')))
+            league_button.click()
 
-        #Choose your betting market and initialize store
-        markets = ['Over/Under 2.5 Goals', 'Both teams to Score?']
-        dict_odds = {}
-        #scraping the betting markets we chose
-        for i, market in enumerate(markets):
-            dropdown = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'marketchooser-container')))
-            dropdown.click()
-            chooser = WebDriverWait(dropdown, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[contains(text(),'+'"'+str(market)+'"'+')]')))
-            chooser.click()
-            #initialize storagei of data to be scraped
-            list_odds = []
-            teams = []
-            list_dates = []
-            time.sleep(1)
-            #finding the sections. each section contains the matches for a specific date
-            sections = driver.find_elements_by_class_name('section')
-            #looping through each date
-            for section in sections:
-                section_date = []
-                section_date.append(section.find_element_by_class_name('section-header-title').text)
-                #findind rows. each row represent one football match
-                rows = WebDriverWait(section, 5).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'event-information')))
-                for row in rows:
-                    odds = row.find_element_by_xpath('.//div[contains(@class, "runner-list")]')
-                    list_odds.append(odds.text)
-                    teams_container = row.find_element_by_class_name('teams-container').text
-                    teams.append(teams_container)
-                #storing the date of matches in each section
-                section_date = section_date * len(rows)
-                list_dates.append(section_date)
-            #unpacking nested lists
-            list_dates = [element for section in list_dates for element in section]
-            #storing data scraped in dictionaries
-            dict_odds['odds_%s' % i] = list_odds
-            dict_odds['teams_%s' % i] = teams
-            dict_odds['dates_%s' % i] = list_dates
+            #Choose your betting market and initialize store
+            markets = ['Over/Under 2.5 Goals', 'Both teams to Score?']
+            dict_odds = {}
+            #scraping the betting markets we chose
+            for i, market in enumerate(markets):
+                dropdown = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, 'marketchooser-container')))
+                dropdown.click()
+                chooser = WebDriverWait(dropdown, 5).until(EC.element_to_be_clickable((By.XPATH, '//*[contains(text(),'+'"'+str(market)+'"'+')]')))
+                chooser.click()
+                #initialize storagei of data to be scraped
+                list_odds = []
+                teams = []
+                list_dates = []
+                time.sleep(1)
+                #finding the sections. each section contains the matches for a specific date
+                sections = driver.find_elements_by_class_name('section')
+                #looping through each date
+                for section in sections:
+                    section_date = []
+                    section_date.append(section.find_element_by_class_name('section-header-title').text)
+                    #findind rows. each row represent one football match
+                    rows = WebDriverWait(section, 5).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, 'event-information')))
+                    for row in rows:
+                        odds = row.find_element_by_xpath('.//div[contains(@class, "runner-list")]')
+                        list_odds.append(odds.text)
+                        teams_container = row.find_element_by_class_name('teams-container').text
+                        teams.append(teams_container)
+                    #storing the date of matches in each section
+                    section_date = section_date * len(rows)
+                    list_dates.append(section_date)
+                #unpacking nested lists
+                list_dates = [element for section in list_dates for element in section]
+                #storing data scraped in dictionaries
+                dict_odds['odds_%s' % i] = list_odds
+                dict_odds['teams_%s' % i] = teams
+                dict_odds['dates_%s' % i] = list_dates
 
-        driver.quit()
+            driver.quit()
 
-        #making dataframes for each league scraped (inside the loop)
-        df_over_under = pd.DataFrame({'Dates':dict_odds['dates_0'], 'Teams':dict_odds['teams_0'], 'over2.5':dict_odds['odds_0']}).set_index(['Teams', 'Dates'])
-        df_btts = pd.DataFrame({'Dates':dict_odds['dates_1'], 'Teams':dict_odds['teams_1'], 'btts':dict_odds['odds_1']}).set_index(['Teams', 'Dates'])
-        #concatenating the dataframes previously created for each betting market
-        df_betfair = pd.concat([df_over_under, df_btts], axis=1, sort=True)
-        df_betfair.reset_index(inplace=True)
-        df_betfair.rename(columns={'index':'Teams'}, inplace=True)
-        #transforming data
-        df_betfair = df_betfair.fillna('')
-        df_betfair = df_betfair.replace('SUSPENDED\n', '', regex=True)
-        df_betfair = df_betfair.applymap(lambda x: x.strip() if isinstance(x, str) else x) #14.0\n
+            #making dataframes for each league scraped (inside the loop)
+            df_over_under = pd.DataFrame({'Dates':dict_odds['dates_0'], 'Teams':dict_odds['teams_0'], 'over2.5':dict_odds['odds_0']}).set_index(['Teams', 'Dates'])
+            df_btts = pd.DataFrame({'Dates':dict_odds['dates_1'], 'Teams':dict_odds['teams_1'], 'btts':dict_odds['odds_1']}).set_index(['Teams', 'Dates'])
+            #concatenating the dataframes previously created for each betting market
+            df_betfair = pd.concat([df_over_under, df_btts], axis=1, sort=True)
+            df_betfair.reset_index(inplace=True)
+            df_betfair.rename(columns={'index':'Teams'}, inplace=True)
+            #transforming data
+            df_betfair = df_betfair.fillna('')
+            df_betfair = df_betfair.replace('SUSPENDED\n', '', regex=True)
+            df_betfair = df_betfair.applymap(lambda x: x.strip() if isinstance(x, str) else x) #14.0\n
 
-        #using time library to replace words "In-Play", "Today" and "Tomorrow" for numeric date
-        today = datetime.date.today()
-        tomorrow = datetime.date.today() + datetime.timedelta(days=1)
-        year = today.strftime("%Y")
-        df_betfair['Dates'] = df_betfair['Dates'].apply(lambda x: re.sub('In-Play', today.strftime("%A, %d %B"), x))
-        df_betfair['Dates'] = df_betfair['Dates'].apply(lambda x: re.sub('Today', today.strftime("%A, %d %B"), x))
-        df_betfair['Dates'] = df_betfair['Dates'].apply(lambda x: re.sub('Tomorrow', tomorrow.strftime("%A, %d %B"), x))
-        df_betfair['Dates'] = df_betfair['Dates'].apply(lambda x: x.split(',')[1].strip())
-        df_betfair['Dates'] = df_betfair['Dates'].apply(lambda x: datetime.datetime.strptime(year + ' ' + x, '%Y %d %B'))
+            #using time library to replace words "In-Play", "Today" and "Tomorrow" for numeric date
+            df_betfair['Dates'] = df_betfair['Dates'].apply(lambda x: re.sub('In-Play', today.strftime("%A, %d %B"), x))
+            df_betfair['Dates'] = df_betfair['Dates'].apply(lambda x: re.sub('Today', today.strftime("%A, %d %B"), x))
+            df_betfair['Dates'] = df_betfair['Dates'].apply(lambda x: re.sub('Tomorrow', tomorrow.strftime("%A, %d %B"), x))
+            df_betfair['Dates'] = df_betfair['Dates'].apply(lambda x: x.split(',')[1].strip())
+            df_betfair['Dates'] = df_betfair['Dates'].apply(lambda x: datetime.datetime.strptime(year + ' ' + x, '%Y %d %B'))
 
-        #storing dataframe of each league in dictionary
-        dict_frames[dict_countries[country][league]] = df_betfair
+            #storing dataframe of each league in dictionary
+            dict_frames[dict_countries[country][league]] = df_betfair
+        except:
+            print(league)
 
 end_time = time.time()
 print('Total time: ' + str(round(end_time-start_time, 1)))
